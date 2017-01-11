dcf6549b6333f007e5feb31886bfc07742c1f562
Kenan Warren <kenan.warren@escribir.io>
2017-01-11 12:06:39 -0500

    More specific

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :speakeasy, Speakeasy.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "herokuapp.com", port: 80],
+  url: [host: "speakeasy42.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
