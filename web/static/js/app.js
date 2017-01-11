fc2d589770f060b279c2cf5d65b504787788ba30
Kenan Warren <kenan.warren@escribir.io>
2017-01-10 22:38:12 -0500

    Remove useless imports

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,10 +2,7 @@ import "phoenix_html"
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import Speakeasy from "../components/speakeasy.vue"
-import socket from "./socket"
-import {Presence} from "phoenix"
-import Peer from "simple-peer"
-import getUserMedia from "getusermedia"
+
 
 Vue.use(VueMaterial)
 // Create the main component
